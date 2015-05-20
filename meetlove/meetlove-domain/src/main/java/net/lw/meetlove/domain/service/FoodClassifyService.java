2b0695a983fd604f90c558453597b8fda47aa8db
publicliu <publicliu@yeah.net>
2015-05-20 21:19:25 +0800

    code

---
+++ meetlove/meetlove-domain/src/main/java/net/lw/meetlove/domain/service/FoodClassifyService.java
@@ -0,0 +1,16 @@
+/**
+ * liuwei
+ * publicliu@yeah.net
+ * 2015年5月20日 下午9:18:34
+ */
+package net.lw.meetlove.domain.service;
+
+import net.lw.meetlove.api.service.IFoodClassifyService;
+
+/**
+ * @author liuwei
+ *
+ */
+public class FoodClassifyService implements IFoodClassifyService {
+
+}
---
+++ meetlove/meetlove-domain/src/main/java/net/lw/meetlove/domain/service/FoodInfoService.java
@@ -0,0 +1,16 @@
+/**
+ * liuwei
+ * publicliu@yeah.net
+ * 2015年5月20日 下午9:18:52
+ */
+package net.lw.meetlove.domain.service;
+
+import net.lw.meetlove.api.service.IFoodInfoService;
+
+/**
+ * @author liuwei
+ *
+ */
+public class FoodInfoService implements IFoodInfoService {
+
+}
